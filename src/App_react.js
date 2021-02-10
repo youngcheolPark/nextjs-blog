2d6e4e6a20a6fc4ba34a2041c7a92729a928d00c
朴　永喆 <01020416@CA4151.local>
2021-02-10 09:35:06 +0900

    init

---
+++ src/App_react.js
@@ -0,0 +1,128 @@
+import { Component } from 'react';
+import Toc from './components/Toc';
+import Subject from './components/Subject';
+import Control from './components/Control';
+import ReadContent from './components/ReadContent';
+import './App.css';
+import CreateContent from './components/CreateContent';
+import UpdateContent from './components/UpdateContent';
+
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.max_content_id=3;
+    this.state = {
+      mode:'welcome',
+      selected_content_id:0,
+      subject:{title:'WEB', sub:'World wide web2'},
+      welcome:{titile:'Welcome', desc:'Hello, React'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML is for imformation'},
+        {id:2, title:'CSS', desc:'CSS is for Desing'},
+        {id:3, title:'Javascript', desc:'Javascript is for interactive'}
+      ]
+    }
+  }
+
+  getReadContent(){
+    let i = 0;
+      while(i<this.state.contents.length){
+        let data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          return data;
+        }
+        i = i +1;
+      }
+  }
+
+  getContent(){
+    var _title, _desc, _article;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.titile;
+      _desc = this.state.welcome.desc;
+      _article = <ReadContent title={_title} sub={_desc}></ReadContent>;
+    }else if(this.state.mode === 'read'){
+      let _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} sub={_content.desc}></ReadContent>;
+    }else if(this.state.mode === 'create'){
+      _article = <CreateContent onSubmit={function (_title, _desc) {
+        // this.setState
+        this.max_content_id = this.max_content_id +1;
+        // this.state.contents.push({id:max_content_id, titile:_title, desc:_desc});
+        // pushはstateを変えてしまう
+        // concatは新たなObjectを生成し、返却する
+        // var _contents = this.state.contents.concat({id:this.max_content_id, title:_title, desc:_desc});
+        var newContent = Array.from(this.state.contents);
+        newContent.push({id:this.max_content_id, title:_title, desc:_desc});
+        this.setState({
+          contents:newContent,
+          mode:'read',
+          selected_content_id:this.max_content_id
+        });
+      }.bind(this)}></CreateContent>;
+    }else if(this.state.mode === 'update'){
+      let _content = this.getReadContent();
+
+      _article = <UpdateContent data={_content} 
+                                onSubmit={function (_id, _title, _desc) {
+        var _contents = Array.from(this.state.contents);
+        console.log(_id, _title, _desc);
+        var i = 0;
+        while(i < _contents.length){
+          if(_contents[i].id === _id){
+            _contents[i] = {id:_id, title:_title, desc:_desc};
+            break;
+          }
+          i = i +1;
+        }
+        this.setState({
+          contents:_contents,
+          mode:'read'
+        });
+      }.bind(this)}></UpdateContent>;
+    }
+
+    return _article;
+  }
+
+  render(){
+    console.log('APP render');
+    console.log('APP this:', this);
+
+    return(
+      <div className="App">
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub} onChangePage={function(){
+          this.setState({mode:'welcome'});
+        }.bind(this)}></Subject>
+        <Toc onChangePage={function(id){
+          this.setState({mode:'read', selected_content_id:Number(id)});
+        }.bind(this)} data={this.state.contents}></Toc>
+        <Control onChangeMode={function(_mode){
+          if(_mode === 'delete'){
+            if(window.confirm('削除してよろしいですか？')){
+              var _contents = Array.from(this.state.contents);
+              var i = 0;
+              while(i < _contents.length){
+                if(_contents[i].id === this.state.selected_content_id){
+                  _contents.splice(i, 1);
+                  break;
+                }
+                i = i +1;
+              }
+              this.setState({
+                contents:_contents,
+                mode:'welcome'
+              });
+            }
+          }else{
+            this.setState({mode:_mode});
+          }          
+        }.bind(this)}></Control>
+        {this.getContent()}
+      </div>
+    );
+  }
+}
+
+export default App;
